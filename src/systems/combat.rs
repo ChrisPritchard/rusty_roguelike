bf2bfa02ae9d9a24eaa81c18777b0070a48dd192
Christopher <chris@grislygrotto.nz>
2023-03-16 12:09:23 +1300

    player is not deleted when health is 0

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(WantsToAttack)]
+#[read_component(Player)]
 #[write_component(Health)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let victims: Vec<(Entity, Entity)> = 
@@ -9,9 +10,10 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
             .map(|(message, attack_order)| (*message, attack_order.victim))
             .collect();
     for (message, victim) in victims {
-        if let Ok(mut health) = ecs.entry_mut(victim).unwrap().get_component_mut::<Health>() {
+        let mut victim_entity = ecs.entry_mut(victim).unwrap();
+        if let Ok(mut health) = victim_entity.get_component_mut::<Health>() {
             health.current -= 1;
-            if health.current < 1 {
+            if health.current < 1 && victim_entity.get_component::<Player>().is_err() {
                 commands.remove(victim);
             }
         }
