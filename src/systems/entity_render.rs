9273ad50f3aea02fb9df52172b822b342942425b
Christopher <chris@grislygrotto.nz>
2023-03-20 09:44:16 +1300

    monsters can no longer be seen in the darkness

--- src/systems/entity_render.rs
+++ src/systems/entity_render.rs
@@ -3,14 +3,20 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Render)]
+#[read_component(FieldOfView)]
+#[read_component(Player)]
 pub fn entity_render(ecs: &SubWorld, #[resource] camera: &Camera) {
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(1);
 
     let offset = Point::new(camera.left_x, camera.top_y);
 
+    let visible = <(&Player, &FieldOfView)>::query().iter(ecs).map(|(_, f)| &f.visible_tiles).nth(0).unwrap();
+
     <(&Point, &Render)>::query().iter(ecs).for_each(|(pos, render)| {
-        draw_batch.set(*pos - offset, render.color, render.glyph);
+        if visible.contains(pos) {
+            draw_batch.set(*pos - offset, render.color, render.glyph);
+        }
     });
 
     draw_batch.submit(5000).expect("Batch error");
