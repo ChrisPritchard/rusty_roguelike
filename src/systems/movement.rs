0ba445434f311be3918e9650b7a88acb28c86691
Christopher <chris@grislygrotto.nz>
2023-03-21 08:16:51 +1300

    only players can reveal tiles

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -20,16 +20,19 @@ pub fn movement(
         // taking advantage of the components on an entity basically being a type set
         commands.add_component(want_move.entity, want_move.destination); 
         
-        if let Ok(fov) = ecs.entry_ref(want_move.entity).unwrap().get_component::<FieldOfView>() {
-            commands.add_component(want_move.entity, fov.clone_dirty()); 
-            for p in fov.visible_tiles.iter() {
+        let entry = ecs.entry_ref(want_move.entity).unwrap();
+        let fov = entry.get_component::<FieldOfView>();
+
+        if entry.get_component::<Player>().is_ok() {
+            camera.on_player_move(want_move.destination);
+            for p in fov.unwrap().visible_tiles.iter() {
                 let idx = map.point2d_to_index(*p);
                 map.revealed[idx] = true;
             }
         }
-
-        if ecs.entry_ref(want_move.entity).unwrap().get_component::<Player>().is_ok() {
-            camera.on_player_move(want_move.destination);
+        
+        if let Ok(fov) = fov {
+            commands.add_component(want_move.entity, fov.clone_dirty()); 
         }
     }
     commands.remove(*entity);
