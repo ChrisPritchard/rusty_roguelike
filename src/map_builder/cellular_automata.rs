daaf7f5fabab8995c6a53f6d73d068f0afaf6f5f
Christopher <chris@grislygrotto.nz>
2023-03-21 16:01:07 +1300

    more automata functions

--- src/map_builder/cellular_automata.rs
+++ src/map_builder/cellular_automata.rs
@@ -14,6 +14,51 @@ impl CellularAutomataArchitect {
             }
         });
     }
+
+    fn count_neighbours(&self, x: i32, y: i32, map: &Map) -> i32 {
+        let mut count = 0;
+        for dx in -1..=1 {
+            for dy in -1..=1 {
+                if dx == 0 && dy == 0 {
+                    continue;
+                }
+                let p = Point::new(x+dx, y+dy);
+                if map.in_bounds(p) && map.tiles[map.point2d_to_index(p)] == TileType::Wall {
+                    count += 1;
+                }
+            }
+        }
+        count
+    }
+
+    fn iteration(&mut self, map: &mut Map) {
+        let mut new_tiles = map.tiles.clone();
+        for y in 1..SCREEN_HEIGHT-1 {
+            for x in 1..SCREEN_WIDTH-1 {
+                let neighbours = self.count_neighbours(x, y, map);
+                let idx = map_idx(x, y);
+                if neighbours > 4 || neighbours == 0 {
+                    new_tiles[idx] = TileType::Wall;
+                } else {
+                    new_tiles[idx] = TileType::Floor;
+                }
+            }
+        }
+        map.tiles = new_tiles;
+    }
+
+    fn find_start(&self, map: &Map) -> Point {
+        let center = Point::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+        let closest_point = map.tiles
+            .iter()
+            .enumerate()
+            .filter(|(_, t)| **t == TileType::Floor)
+            .map(|(idx, _)| (idx, DistanceAlg::Pythagoras.distance2d(center, map.index_to_point2d(idx))))
+            .min_by(|(_, distance), (_, distance2)| distance.partial_cmp(&distance2).unwrap())
+            .map(|(idx, _)| idx)
+            .unwrap();
+        map.index_to_point2d(closest_point)
+    }
 }
 
 impl MapArchitect for CellularAutomataArchitect {
